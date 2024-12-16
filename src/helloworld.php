2cffbc9ed33e4ca7a114901066b9c666592a8348
richard-commerz <richard@commerz.se>
2024-12-16 16:12:42 +0100

    Fix namespace for helloworld class in composer.json and src/helloworld.php

--- src/helloworld.php
+++ src/helloworld.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace src;
+
+// composer require commerzsylius/helloworld
+// Plugin commerzsylius/helloworld could not be initialized, class not found: src\helloworld
+
+// See "composer.json" To fix this error!!
+
 class helloworld
 {
     public function sayHello()
